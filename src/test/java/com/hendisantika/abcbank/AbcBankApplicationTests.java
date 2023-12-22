a7a9056a9b16b9bbf50789c55623c67fc0f1e974
hendisantika <hendisantika@yahoo.co.id>
2023-12-23 05:46:06 +0700

    Modify AbcBankApplicationTests 🫸🌀✏️ 📗 :octocat:

--- src/test/java/com/hendisantika/abcbank/AbcBankApplicationTests.java
+++ src/test/java/com/hendisantika/abcbank/AbcBankApplicationTests.java
@@ -2,8 +2,10 @@ package com.hendisantika.abcbank;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
-@SpringBootTest
+@SpringBootTest(classes = {AbcBankApplication.class}, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ActiveProfiles(profiles = {"test"})
 class AbcBankApplicationTests {
 
     @Test
