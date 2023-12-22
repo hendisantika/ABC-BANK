bb8bf0f3cae7c286692b8da3be8cfea7fd40f292
hendisantika <hendisantika@yahoo.co.id>
2023-12-22 09:32:06 +0700

    Add SwaggerConfig 🫸🌀✏️ 📗 :octocat:

---
+++ src/main/java/com/hendisantika/abcbank/config/SwaggerConfig.java
@@ -0,0 +1,50 @@
+package com.hendisantika.abcbank.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.info.License;
+import org.springdoc.core.models.GroupedOpenApi;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ABC-BANK
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/22/23
+ * Time: 09:30
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class SwaggerConfig {
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("abc-bank-service")
+                .pathsToMatch("/**")
+                .build();
+    }
+
+    @Bean
+    public OpenAPI customOpenAPI(@Value("${application-description}") String appDescription, @Value("${application" +
+            "-version}") String appVersion) {
+        Contact contact = new Contact();
+        contact.setEmail("hendisantika@yahoo.co.id");
+        contact.setName("HENDI - IT DEV Team");
+        contact.setUrl("https://s.id/hendisantika");
+        return new OpenAPI()
+                .info(new Info()
+                        .title("ABC BANK Base Service Application API")
+                        .version(appVersion)
+                        .description(appDescription)
+                        .termsOfService("http://swagger.io/terms/")
+                        .license(new License().name("Apache 2.0").url("http://springdoc.org"))
+                        .contact(contact)
+                );
+
+    }
+}
