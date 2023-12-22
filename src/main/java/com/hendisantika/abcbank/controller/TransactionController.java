2d54738f16d961dec60a9d94f3407de5b887ae9f
hendisantika <hendisantika@yahoo.co.id>
2023-12-22 09:38:19 +0700

    Add withdraw in TransactionController 🫸🌀✏️ 📗 :octocat:

--- src/main/java/com/hendisantika/abcbank/controller/TransactionController.java
+++ src/main/java/com/hendisantika/abcbank/controller/TransactionController.java
@@ -1,15 +1,18 @@
 package com.hendisantika.abcbank.controller;
 
+import com.hendisantika.abcbank.model.WithdrawFromAccountModel;
 import com.hendisantika.abcbank.service.AccountTransactionService;
+import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.modelmapper.ModelMapper;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -33,4 +36,11 @@ public class TransactionController {
     private final AccountTransactionService txService;
 
     private final ModelMapper modelMapper;
+
+    @Operation(summary = "withdraw amount")
+    @PostMapping(value = "/withdraw")
+    public ResponseEntity<?> withdraw(@Valid @RequestBody WithdrawFromAccountModel reqModel) {
+        txService.withdrawal(reqModel.getFromAccountNumber(), reqModel.getWithdrawlAmount());
+        return new ResponseEntity<>(HttpStatus.OK);
+    }
 }
