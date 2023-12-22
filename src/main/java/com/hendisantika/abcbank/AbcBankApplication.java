c36f6fab24e24e57e2d1b45dd6fc1f22f6d27795
hendisantika <hendisantika@yahoo.co.id>
2023-12-22 08:29:03 +0700

    Create starter project 🫸🌀✏️ 📗 :octocat:

---
+++ src/main/java/com/hendisantika/abcbank/AbcBankApplication.java
@@ -0,0 +1,13 @@
+package com.hendisantika.abcbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AbcBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AbcBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hendisantika/abcbank/AbcBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hendisantika.abcbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AbcBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
