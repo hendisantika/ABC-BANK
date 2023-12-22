b4db3d1c9d438ae73332e5bd935463996ac9e94f
hendisantika <hendisantika@yahoo.co.id>
2023-12-23 05:53:22 +0700

    Add createAccountTest in AccountControllerTest 🫸🌀✏️ 📗 :octocat:

--- src/test/java/com/hendisantika/abcbank/controller/AccountControllerTest.java
+++ src/test/java/com/hendisantika/abcbank/controller/AccountControllerTest.java
@@ -3,15 +3,26 @@ package com.hendisantika.abcbank.controller;
 import com.hendisantika.abcbank.AbcBankApplication;
 import com.hendisantika.abcbank.entity.Account;
 import com.hendisantika.abcbank.service.AccountService;
+import com.hendisantika.abcbank.util.AppUtil;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.data.web.PageableHandlerMethodArgumentResolver;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.test.web.servlet.RequestBuilder;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -55,5 +66,20 @@ public class AccountControllerTest {
                 .setCustomArgumentResolvers(new PageableHandlerMethodArgumentResolver()).build();// Standalone context
 
         account = Account.builder().acountHolder("test user").balance(new BigDecimal("89.00")).build();
+    }
+
+    @Test
+    public void createAccountTest() throws Exception {
+        Mockito.when(accountService.saveUpdate(Mockito.any())).thenReturn(account);
+        Mockito.when(accountService.findByAccountNumber(Mockito.any())).thenReturn(account);
+        RequestBuilder requestBuilder = MockMvcRequestBuilders.get("/accounts")
+                .accept(MediaType.APPLICATION_JSON, MediaType.ALL);
+        MvcResult result = mockMvc.perform(requestBuilder).andReturn();
+        ResponseEntity<Account> responseEntity = this.testRestTemplate.exchange(
+                "/accounts/" + account.getAccountNumber(),
+                HttpMethod.GET, AppUtil.getHttpHeader(),
+                Account.class);
+        Assertions.assertEquals(HttpStatus.OK, responseEntity.getStatusCode());
+        Assertions.assertEquals(account, responseEntity.getBody());
     }
 }
