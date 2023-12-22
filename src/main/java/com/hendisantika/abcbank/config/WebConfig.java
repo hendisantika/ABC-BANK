646eafcb1fbc44e7b4c817578e5a8bda6e04e72d
hendisantika <hendisantika@yahoo.co.id>
2023-12-22 09:30:01 +0700

    Add WebConfig 🫸🌀✏️ 📗 :octocat:

---
+++ src/main/java/com/hendisantika/abcbank/config/WebConfig.java
@@ -0,0 +1,27 @@
+package com.hendisantika.abcbank.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Created by IntelliJ IDEA.
+ * Project : ABC-BANK
+ * User: hendisantika
+ * Email: hendisantika@gmail.com
+ * Telegram : @hendisantika34
+ * Date: 12/22/23
+ * Time: 09:29
+ * To change this template use File | Settings | File Templates.
+ */
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        AntPathMatcher matcher = new AntPathMatcher();
+        matcher.setCaseSensitive(false);
+        configurer.setPathMatcher(matcher);
+    }
+}
