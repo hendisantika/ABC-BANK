c041ef0d4685713bf2f37adc93ead2fe5f7440e1
hendisantika <hendisantika@yahoo.co.id>
2023-12-22 09:27:19 +0700

    Add modelMapper in Main App 🫸🌀✏️ 📗 :octocat:

--- src/main/java/com/hendisantika/abcbank/AbcBankApplication.java
+++ src/main/java/com/hendisantika/abcbank/AbcBankApplication.java
@@ -1,7 +1,12 @@
 package com.hendisantika.abcbank;
 
+import org.modelmapper.ModelMapper;
+import org.modelmapper.config.Configuration;
+import org.modelmapper.convention.MatchingStrategies;
+import org.modelmapper.convention.NamingConventions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class AbcBankApplication {
@@ -10,4 +15,14 @@ public class AbcBankApplication {
         SpringApplication.run(AbcBankApplication.class, args);
     }
 
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        modelMapper.getConfiguration().setFieldMatchingEnabled(true).setFieldAccessLevel(Configuration.AccessLevel.PRIVATE)
+                .setMatchingStrategy(MatchingStrategies.LOOSE)
+                .setSourceNamingConvention(NamingConventions.JAVABEANS_ACCESSOR)
+                .setDestinationNamingConvention(NamingConventions.JAVABEANS_ACCESSOR);
+
+        return modelMapper;
+    }
 }
