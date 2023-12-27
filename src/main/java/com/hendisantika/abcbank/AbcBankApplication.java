201500e8ca9dbe1797788ff1cb68915dd5cd7ecb
armen <armen.arzumanyan@gmail.com>
2023-12-27 16:22:20 +0400

    removed additional declarations

--- src/main/java/com/hendisantika/abcbank/AbcBankApplication.java
+++ src/main/java/com/hendisantika/abcbank/AbcBankApplication.java
@@ -6,15 +6,9 @@ import org.modelmapper.convention.MatchingStrategies;
 import org.modelmapper.convention.NamingConventions;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
-@EnableJpaRepositories("com.hendisantika.abcbank.repository")
-@ComponentScan(basePackages = {"com.hendisantika"})
-@EntityScan("com.hendisantika.abcbank.entity")
 public class AbcBankApplication {
 
     public static void main(String[] args) {
